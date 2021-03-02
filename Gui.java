55257b74ae7d4f98cfb6c8e91c5b8c21f79e2687
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-03-03 00:34:15 +0100

    Update Gui.java

--- Gui.java
+++ Gui.java
@@ -82,7 +82,7 @@ import java.awt.event.ActionListener;
         }
 
         public void setShowInventory(Inventory b) {
-            this.inventory.setText("Theres is 3 NPC´s (S,J,J) Check your console in the background to know there postion");
+            this.inventory.setText("You are Ola, Theres is 3 NPC´s (S,J,J) Check your console in the background to know there postion");
         }
 
         public void setShowPlayer(Person p, Room room, int position) {
