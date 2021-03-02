964107bf2b092d91e1aa77143e2781170a158175
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-03-02 23:31:44 +0100

    Add files via upload

--- Person.java
+++ Person.java
@@ -1,20 +1,26 @@
 package Projekt2;
 
-public class Person extends Npc{
-    
-	 
+import java.util.*;
 
-    public Person(String name) {
-        super(name);
+
+public class Person extends Npc implements Runnable{
+
+    private int position;
+    public Person(String name, int startRoom, Gui m) {
+        super(name, m);
+        this.position = startRoom;
     }
-
-	public void giveObject() {
-	}
-
-	
-
-
-	
-   
-   
+    public synchronized void move(){
+        int slump = new Random().nextInt(4);
+        this.position = slump;
+        System.out.println("Moving " + this.name + " to Room " + (this.position + 1) );
+    }
+    public int getPosition() {
+        return position;
+    }
+    
+    @Override
+   public void run(){
+        move();
+   }
 }
