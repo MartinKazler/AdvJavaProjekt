9ca1a642fcce1f98e244c7c44164075993321c48
MartinKazler <70008408+MartinKazler@users.noreply.github.com>
2021-03-04 11:55:06 +0100

    Update Player.java

--- Player.java
+++ Player.java
@@ -3,7 +3,11 @@ package Projekt2;
 public class Player extends Npc {   
 
 
-    public Player(String name2) {
-        super(name2, gui);
+    public Player(String name) {
+        super(name, gui);
     }
+
+	public void run() {
+
+	}
 }
